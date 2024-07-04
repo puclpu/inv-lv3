1eb139237357653b37df2c9f6bba631c341fc706
kimchaerin <puclpu18@gmail.com>
2024-07-04 22:21:14 +0900

    🐛TeacherRequest 유효성 검증 기능 추가
    - TeacherRequestDTO에 NotBlank 어노테이션과 message 추가
    - TeacherController에 Valid 어노테이션 추가

--- back-office/src/main/java/com/sparta/backoffice/teacher/controller/TeacherController.java
+++ back-office/src/main/java/com/sparta/backoffice/teacher/controller/TeacherController.java
@@ -5,6 +5,7 @@ import com.sparta.backoffice.teacher.domain.Teacher;
 import com.sparta.backoffice.teacher.dto.TeacherRequestDTO;
 import com.sparta.backoffice.teacher.dto.TeacherResponseDTO;
 import com.sparta.backoffice.teacher.service.TeacherService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -20,14 +21,14 @@ public class TeacherController {
 
     @Secured({Role.Authority.STAFF, Role.Authority.MANAGER})
     @PostMapping()
-    public ResponseEntity<TeacherResponseDTO> createTeacher(@RequestBody TeacherRequestDTO requestDTO) {
+    public ResponseEntity<TeacherResponseDTO> createTeacher(@RequestBody @Valid TeacherRequestDTO requestDTO) {
         TeacherResponseDTO responseDTO = teacherService.createTeacher(requestDTO);
         return new ResponseEntity<>(responseDTO, HttpStatus.OK);
     }
 
     @Secured(Role.Authority.MANAGER)
     @PutMapping("/{teacherId}")
-    public ResponseEntity<TeacherResponseDTO> updateTeacher(@RequestBody TeacherRequestDTO requestDTO, @PathVariable Long teacherId) {
+    public ResponseEntity<TeacherResponseDTO> updateTeacher(@RequestBody @Valid TeacherRequestDTO requestDTO, @PathVariable Long teacherId) {
         TeacherResponseDTO responseDTO = teacherService.updateTeacher(requestDTO, teacherId);
         return new ResponseEntity<>(responseDTO, HttpStatus.OK);
     }
