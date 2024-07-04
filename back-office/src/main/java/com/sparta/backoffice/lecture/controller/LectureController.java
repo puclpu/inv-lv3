a3550eebbf8990d2c5ff4f05059da08d7ccc1ca9
kimchaerin <puclpu18@gmail.com>
2024-07-04 19:14:19 +0900

    🔨LectureController RequestMapping url 변경
    - REST api 설계 규칙에 맞는 url로 변경

--- back-office/src/main/java/com/sparta/backoffice/lecture/controller/LectureController.java
+++ back-office/src/main/java/com/sparta/backoffice/lecture/controller/LectureController.java
@@ -15,7 +15,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/admins/lecture")
+@RequestMapping("/admins/lectures")
 @RequiredArgsConstructor
 public class LectureController {
 
